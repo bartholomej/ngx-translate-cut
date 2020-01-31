472b405e1674f328005a9cb6f8ae8c70fd0758f1
BART! <bart@bartweb.cz>
2020-01-31 15:47:01 +0100

    feat: handle edge cases

--- projects/ngx-translate-cut/src/lib/ngx-translate-cut.pipe.ts
+++ projects/ngx-translate-cut/src/lib/ngx-translate-cut.pipe.ts
@@ -1,11 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const SEPARATOR = '|';
+
 @Pipe({
   name: 'translateCut'
 })
 export class NgxTranslateCutPipe implements PipeTransform {
-  public transform(value: string, index: string | number): string {
+  public transform(value: string, index: number): string {
     const cutIndex = Number(index);
-    return value.split('|')[cutIndex] || '';
+    const splitted: string[] = value ? value.split(SEPARATOR) : null;
+    const phrase: string = splitted ? splitted[cutIndex] : null;
+    const result = phrase ? phrase.trim() : '';
+
+    return result;
   }
 }
