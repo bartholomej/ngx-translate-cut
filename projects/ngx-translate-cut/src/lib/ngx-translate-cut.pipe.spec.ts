346b2a2d749fbd93aa9827966037aeb0b8d2d044
BART! <bart@bartweb.cz>
2020-01-31 15:46:30 +0100

    test(pipe): add tests for pipe

--- projects/ngx-translate-cut/src/lib/ngx-translate-cut.pipe.spec.ts
+++ projects/ngx-translate-cut/src/lib/ngx-translate-cut.pipe.spec.ts
@@ -1,10 +1,47 @@
 /* tslint:disable:no-unused-variable */
-
 import { NgxTranslateCutPipe } from './ngx-translate-cut.pipe';
 
-describe('Pipe: NgxTranslateCutPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NgxTranslateCutPipe();
-    expect(pipe).toBeTruthy();
+// const data: string = [1, 2, 3].map((i) => `text ${i}`).join('|');
+const data = 'first | second | last';
+
+describe('NgxTranslateCutPipe', () => {
+  let pipe: NgxTranslateCutPipe;
+
+  beforeEach(() => {
+    pipe = new NgxTranslateCutPipe();
+  });
+
+  it('First', () => {
+    expect(pipe.transform(data, 0))
+      .toEqual('first');
+  });
+
+  it('Second', () => {
+    expect(pipe.transform(data, 1))
+      .toEqual('second');
+  });
+
+  it('Last', () => {
+    expect(pipe.transform(data, 2))
+      .toEqual('last');
+  });
+
+  it('Out of array', () => {
+    expect(pipe.transform(data, 3))
+      .toEqual('');
+
+    expect(pipe.transform(data, -1))
+      .toEqual('');
+  });
+
+  it('Empty', () => {
+    expect(pipe.transform('', 0))
+      .toEqual('');
+
+    expect(pipe.transform(undefined, 0))
+      .toEqual('');
+
+    expect(pipe.transform(null, 0))
+      .toEqual('');
   });
 });
