6a87ae4ccc76de109a7464fd89c324cdd22cfdbf
BART! <bart@bartweb.cz>
2020-02-02 20:50:17 +0100

    tests: add ci configuration

--- projects/demo/src/app/app.component.spec.ts
+++ projects/demo/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { async, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgxTranslateCutModule } from 'projects/ngx-translate-cut/src/public-api';
 import { AppComponent } from './app.component';
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot(), NgxTranslateCutModule],
       declarations: [
         AppComponent
       ],
@@ -15,4 +19,12 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it(`should get github link`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.github-logo').href).toContain('https://github.com/bartholomej/ngx-translate-cut');
+  }));
 });
+
