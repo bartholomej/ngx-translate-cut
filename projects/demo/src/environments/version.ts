f8ee277c16d98ede1db7c396eb09fff7318c8208
BART! <bart@bartweb.cz>
2020-07-25 20:16:01 +0200

    chore(version): gitnore version.ts file

--- projects/demo/src/environments/version.ts
+++
@@ -1,7 +0,0 @@
-// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
-/* tslint:disable */
-export const VERSION = {
-  TAG: '1.5.0',
-  COMMIT_HASH: '',
-};
-/* tslint:enable */
