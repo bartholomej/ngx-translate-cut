4c9c2c1164b00062b70a4b2af2eb265820be8eb1
BART! <bart@bartweb.cz>
2020-07-26 09:53:15 +0200

    fix(pipe): calling pipe programmatically (separator fallback)

--- projects/ngx-translate-cut/src/lib/ngx-translate-cut.pipe.ts
+++ projects/ngx-translate-cut/src/lib/ngx-translate-cut.pipe.ts
@@ -1,15 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { SEPARATOR } from './ngx-translate-cut.constants';
 import { NgxTranslateCutOptionsService } from './ngx-translate-cut.options.service';
 
 @Pipe({
   name: 'translateCut',
 })
 export class NgxTranslateCutPipe implements PipeTransform {
-  constructor(private options: NgxTranslateCutOptionsService) {}
+  constructor(private options?: NgxTranslateCutOptionsService) {}
 
   public transform(value: string, index: number): string {
     const cutIndex = Number(index);
-    const splitted: string[] = value ? value.split(this.options.separator) : null;
+    const splitted: string[] = value ? value.split(this.options?.separator || SEPARATOR) : null;
     const phrase: string = splitted ? splitted[cutIndex] : null;
     const result = phrase ? phrase.trim() : '';
 
