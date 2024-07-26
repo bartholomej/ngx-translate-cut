218006ac109236d5cf3625266cdf73ea59cae193
Steven <steven.gunneweg@aanzee.nl>
2024-07-26 10:57:41 +0200

    fix(trim): Fixed boolean option condition and add tests

---
+++ projects/ngx-translate-cut/src/lib/ngx-translate-cut.options.factory.spec.ts
@@ -0,0 +1,29 @@
+import { SEPARATOR } from './ngx-translate-cut.constants';
+import { ngxTranslateCutOptionsFactory } from './ngx-translate-cut.options.factory';
+import { NgxTranslateCutOptions } from './ngx-translate-cut.options.interface';
+import { NgxTranslateCutOptionsService } from './ngx-translate-cut.options.service';
+
+describe('ngxTranslateCutOptionsFactory', () => {
+  let factory: (options?: NgxTranslateCutOptions) => NgxTranslateCutOptionsService;
+
+  beforeEach(() => {
+    factory = ngxTranslateCutOptionsFactory;
+  });
+
+  it('should be created with default options', () => {
+    const result = factory();
+
+    expect(result.separator).toEqual(SEPARATOR);
+    expect(result.trim).toEqual(true);
+  });
+
+  it('should be created with custom options', () => {
+    const result = factory({
+      separator: '*',
+      trim: false,
+    });
+
+    expect(result.separator).toEqual('*');
+    expect(result.trim).toEqual(false);
+  });
+});
