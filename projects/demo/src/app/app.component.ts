9f27729435f6e0834adba971149a6fa8b60daa98
BART! <bart@bartweb.cz>
2024-11-28 22:57:15 +0100

    chore(deps): ng update + migrations

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { VERSION } from '../environments/version';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
 export class AppComponent {
   public version = VERSION;
