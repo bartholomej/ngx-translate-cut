94470bf1e75af0a133311ef9982a180b6855d212
BART! <bart@bartweb.cz>
2025-08-18 22:38:49 +0200

    fix(update): ngx-translate 17 migtaion

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,27 +1,18 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NgxTranslateCutModule } from 'projects/ngx-translate-cut/src/lib/ngx-translate-cut.module';
 import { AppComponent } from './app.component';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, 'assets/i18n/');
-}
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    HttpClientModule,
     TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
+      loader: provideTranslateHttpLoader({ prefix: './assets/i18n/', suffix: '.json' }),
     }),
+    BrowserModule,
     NgxTranslateCutModule.forRoot({
       // Pipe `|` is default option. It's just for show and play :)
       // If you don't need to change the separator, you don't have to use it at all.
@@ -29,6 +20,7 @@ export function HttpLoaderFactory(http: HttpClient) {
       separator: '|',
     }),
   ],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
   bootstrap: [AppComponent],
 })
 export class AppModule {
