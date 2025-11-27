2de133a35b6e32dc4312d66662d527f6e62d60b9
BART! <bart@bartweb.cz>
2025-11-27 01:01:12 +0100

    chore(test): vitest config

--- projects/demo/src/app/app.component.spec.ts
+++ projects/demo/src/app/app.component.spec.ts
@@ -1,19 +1,12 @@
-import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { TranslateModule } from '@ngx-translate/core';
-import { NgxTranslateCutOptionsService } from 'projects/ngx-translate-cut/src/lib/ngx-translate-cut.options.service';
-import { NgxTranslateCutModule } from 'projects/ngx-translate-cut/src/public-api';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [NgxTranslateCutOptionsService, provideZonelessChangeDetection()],
-      imports: [TranslateModule.forRoot(), NgxTranslateCutModule],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppModule],
     }).compileComponents();
   });
 
